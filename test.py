da669bd7685503457e6c0c4a20a1f7fbf4ed39ef
FLslemEX <74836140+FLslemEX@users.noreply.github.com>
2021-01-07 23:18:38 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,4 @@
+nameUser=input("Ваше имя")
+cityUser=input("Место жительства")
+oldUser=input("Сколько лет")
+print("Ваше имя {0}. Место жительства{1}.Сколько лет{2}.".format(nameUser,cityUser,oldUser))
